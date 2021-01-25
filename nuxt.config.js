fb8cfc219b0c30feaf64d627f280e8e71fa86eb6
Yoga Pratama <yogamadness@gmail.com>
2021-01-25 13:24:22 +0700

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Vue Material Admin Template',
+    title: 'Admin',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
@@ -58,8 +58,10 @@ module.exports = {
   /*
   ** Nuxt.js modules
   */
-  modules: [],
-
+  modules: ['@nuxtjs/axios'],
+  axios : {
+    
+  },
   /*
   ** Build configuration
   */
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -2,17 +2,76 @@
   <v-container grid-list-xl fluid>
     <h4>
       <i class="fa fa-smile-o"></i>
-      Hi there, this an empty page.</h4>
+      Hi there, this an empty page.
+    </h4>
+
+    <v-card ref="form">
+      <v-card-text>
+        <v-text-field
+          label="Username"
+          placeholder="John Doe"
+          v-model="username"
+          required
+          ref="username"
+          :rules="[() => !!username || 'Di isi woy']"
+          :error-messages="errorMessages"
+        ></v-text-field>
+        <v-text-field
+          label="password"
+          placeholder="John Doe"
+          v-model="password"
+          required
+          ref="password"
+          :rules="[() => !!password || 'Isi Password']"
+          :error-messages="errorMessages"
+        ></v-text-field>
+
+        <v-select
+          autocomplete
+          label="Country"
+          placeholder="Select..."
+          :rules="[() => !!negara || 'This field is required']"
+          :items="countries"
+          v-model="negara"
+          ref="country"
+          required
+          multiple
+        ></v-select>
+
+        <v-btn color="primary" flat @click="simpan">Submit</v-btn>
+
+        <!-- <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" flat @click="simpan" v-bind="attrs" v-on="on"
+              >Submit</v-btn
+            >
+          </template>
+          <span>Tooltip</span>
+        </v-tooltip> -->
+      </v-card-text>
+    </v-card>
+
+    <v-alert type="success"></v-alert>
   </v-container>
 </template>
 
 <script>
-  export default {
-    layout: "dashboard",
-
-  }
+export default {
+  layout: "dashboard",
+  data() {
+    return {
+      username: "",
+      password: "",
+      errorMessages: [],
+      negara: "",
+      countries: ["Indonesia", "Malaysia"],
+    };
+  },
+  methods: {
+    simpan() {},
+  },
+};
 </script>
 
-<style scoped>
-
+<style>
 </style>
